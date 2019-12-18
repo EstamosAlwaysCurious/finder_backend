90166856bb96675e19b89cb3288773d6f7d15b42
ignacio <nachomattos78@gmail.com>
2019-12-18 21:09:23 +0000

    get and post request ok

--- handler.js
+++ handler.js
@@ -1,18 +1,47 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "finder"
+});
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get("/pharmacies", function(req, res) {
+    connection.query("SELECT * FROM pharmacies", function(err, data){
+        if(err){
+            console.log("Error fetching pharmacies", err);
+            res.status(500).json({error: er});
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+app.post("/pharmacies", function(req, res) {
+    const pharmacy = req.body;
+    const sql = "INSERT INTO pharmacies SET ?";
+    connection.query(sql, pharmacy, function (err, data){
+        if(err){
+            res.status(500).json({error: err});
+        }else{
+            pharmacy.id = data.insertId;
+            res.status(201).json(pharmacy);
+        }
+    });
+});
+
+module.exports.pharmacies = serverless(app);
+
+
+
